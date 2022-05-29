5c3fe20aa643d2b05a05578e0cccd5088b825c39
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-29 14:54:26 +0530

    create mainwindow

--- login.php
+++ login.php
@@ -1,7 +1,17 @@
 <?php 
 
-if(isset($_POST('txtUserame'))){
-    include "login.html"
-    die()
+if(!(isset($_POST['txtUserame']) && isset($_POST['txtPassword']))){
+    include "login.html";
+    die();
+}
+
+$username = $_POST['txtUsername'];
+$password = $_POST['txtPassword'];
+
+if ($username == 'root' && $password == '1234') {
+    echo('Hi');
+} else {
+    echo('Incorrect Username or Password');
+    include "login.html";
 }
 ?>
---
+++ mainWindow.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to Harvest Super</h1>
+    <h4>You are login as root</h4>
+    <h4><a href="logout.php">logout</a></h4>
+    <ul>
+        <li><a href="fruit.php">Fruits</a></li>
+        <li><a href="vegetable.php">Vegetables</a></li>
+    </ul>
+</body>
+</html>
